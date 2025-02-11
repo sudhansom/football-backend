9892024105f6f20a373630edd77ad8ce9304a207
bhuwan <bkspoudel@gmail.com>
2025-02-11 22:00:21 +0100

    change authority to change the participation

--- routes/schedule-route.js
+++ routes/schedule-route.js
@@ -4,12 +4,13 @@ const uuid = require('uuid')
 const { getSchedules, createSchedule, updateParticipate } = require('../controller/schedule-controller');
 
 const verifyAdmin = require("../middleware/verifyAdmin.js")
+const verifyToken = require("../middleware/verifyToken.js")
 
 const router = express.Router()
 
 router.get('/', getSchedules);
 router.post('/',verifyAdmin, createSchedule);
-router.patch('/:id',verifyAdmin, updateParticipate);
+router.patch('/:id',verifyToken, updateParticipate);
 
 
 
